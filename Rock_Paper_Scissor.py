e1f24851df2d761ded977a43644aa1ebb3cc4dc1
Harshita Aswani <92624476+HarshitaAswani@users.noreply.github.com>
2023-01-23 17:06:38 +0530

    Add files via upload

---
+++ Rock_Paper_Scissor.py
@@ -0,0 +1,48 @@
+import random
+
+user_wins = 0
+computer_wins = 0
+
+options = ['rock', 'paper', 'scissors']
+
+while True:
+    user_input = input("Type rock/paper/scissors or Q to quit:").lower()
+    if user_input == 'q':
+        break
+    if user_input not in options:
+        continue
+
+    random_number = random.randint(0, 2)
+    # rock:0, paper:1, scissors:2
+
+    computer_pick = options[random_number]
+    print('Computer picked:', computer_pick)
+
+    if user_input == 'rock' and computer_pick == 'scissors':
+        print('You won!')
+        user_wins += 1
+
+    elif user_input == 'scissors' and computer_pick == 'paper':
+        print('You won!')
+        user_wins += 1
+
+    elif user_input == 'paper' and computer_pick == 'rock':
+        print('You won!')
+        user_wins += 1
+
+    elif user_input == 'paper' and computer_pick == 'paper':
+        print('Draw!')
+
+    elif user_input == 'rock' and computer_pick == 'rock':
+        print('Draw!')
+
+    elif user_input == 'scissors' and computer_pick == 'scissors':
+        print('Draw!')
+
+    else:
+        print('You lost!')
+        computer_wins += 1
+
+print("Your score is:", user_wins)
+print("Computer's score is:", computer_wins)
+print("Goodbye!")
